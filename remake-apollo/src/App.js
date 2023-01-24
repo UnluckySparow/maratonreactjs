8170833f2faeb52948563103e0554f78ead00204
UnluckySparow <mouad.ait05@gmail.com>
2023-01-24 23:49:03 +0100

    queries get contact

--- remake-apollo/src/App.js
+++ remake-apollo/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-
+import { GET_CONTACT } from './queries/queries';
+import { useQuery } from '@apollo/react-hooks';
 function App() {
+  const data = useQuery(GET_CONTACT);
+  console.log(data);
   return (
     <div className="App">
       App      
---
+++ remake-apollo/src/queries/queries.js
@@ -0,0 +1,10 @@
+import {gql} from "apollo-boost";
+export const GET_CONTACT = gql`
+query MyQuery {
+    user {
+      mail
+      full_name
+      Carte_national
+    }
+  }
+`;
